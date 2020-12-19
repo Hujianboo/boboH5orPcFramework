9ec808f208dc493e4f476c3ac39a691c2d55e74e
hujianbo <466636709@qq.com>
2020-12-19 19:24:04 +0800

    fix: 删除个人邮箱

--- src/test.js
+++ src/test.js
@@ -1,9 +1,2 @@
-/*
- * @Author: Hujianbo
- * @Date: 2020-12-19 17:16:40
- * @LastEditors: Hujianbo
- * @LastEditTime: 2020-12-19 18:35:13
- * @FilePath: /ObGameFrame/test.js
- */
 let Ob = require('./Init');
 console.log(Ob());
